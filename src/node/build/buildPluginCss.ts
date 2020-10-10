e80b9b5c7e796acbcab585d7e29b2645905566df
Vishnu - PDI <vprabh@pdisoftware.com>
2020-10-10 18:57:45 -0500

    generate css file  at every chunk sans hash

--- src/node/build/buildPluginCss.ts
+++ src/node/build/buildPluginCss.ts
@@ -1,38 +1,38 @@
-import path from 'path'
-import { Plugin } from 'rollup'
-import { resolveAsset, registerAssets } from './buildPluginAsset'
-import { BuildConfig } from '../config'
-import hash_sum from 'hash-sum'
+import path from "path";
+import { Plugin } from "rollup";
+import { resolveAsset, registerAssets } from "./buildPluginAsset";
+import { BuildConfig } from "../config";
+import hash_sum from "hash-sum";
 import {
   urlRE,
   compileCss,
   cssPreprocessLangRE,
   rewriteCssUrls,
   isCSSRequest,
-  cssModuleRE
-} from '../utils/cssUtils'
+  cssModuleRE,
+} from "../utils/cssUtils";
 import {
   SFCStyleCompileResults,
-  SFCAsyncStyleCompileOptions
-} from '@vue/compiler-sfc'
-import chalk from 'chalk'
-import { CssPreprocessOptions } from '../config'
-import { dataToEsm } from '@rollup/pluginutils'
+  SFCAsyncStyleCompileOptions,
+} from "@vue/compiler-sfc";
+import chalk from "chalk";
+import { CssPreprocessOptions } from "../config";
+import { dataToEsm } from "@rollup/pluginutils";
 
-const debug = require('debug')('vite:build:css')
+const debug = require("debug")("vite:build:css");
 
-const cssInjectionMarker = `__VITE_CSS__`
-const cssInjectionRE = /__VITE_CSS__\(\)/g
+const cssInjectionMarker = `__VITE_CSS__`;
+const cssInjectionRE = /__VITE_CSS__\(\)/g;
 
 interface BuildCssOption {
-  root: string
-  publicBase: string
-  assetsDir: string
-  minify?: BuildConfig['minify']
-  inlineLimit?: number
-  cssCodeSplit?: boolean
-  preprocessOptions?: CssPreprocessOptions
-  modulesOptions?: SFCAsyncStyleCompileOptions['modulesOptions']
+  root: string;
+  publicBase: string;
+  assetsDir: string;
+  minify?: BuildConfig["minify"];
+  inlineLimit?: number;
+  cssCodeSplit?: boolean;
+  preprocessOptions?: CssPreprocessOptions;
+  modulesOptions?: SFCAsyncStyleCompileOptions["modulesOptions"];
 }
 
 export const createBuildCssPlugin = ({
@@ -43,165 +43,180 @@ export const createBuildCssPlugin = ({
   inlineLimit = 0,
   cssCodeSplit = true,
   preprocessOptions,
-  modulesOptions = {}
+  modulesOptions = {},
 }: BuildCssOption): Plugin => {
-  const styles: Map<string, string> = new Map()
-  const assets = new Map<string, Buffer>()
-  let staticCss = ''
+  const styles: Map<string, string> = new Map();
+  const assets = new Map<string, Buffer>();
+  let staticCss = "";
 
   return {
-    name: 'vite:css',
+    name: "vite:css",
     async transform(css: string, id: string) {
       if (isCSSRequest(id)) {
         // if this is a Vue SFC style request, it's already processed by
         // rollup-plugin-vue and we just need to rewrite URLs + collect it
-        const isVueStyle = /\?vue&type=style/.test(id)
+        const isVueStyle = /\?vue&type=style/.test(id);
         const preprocessLang = (id.match(cssPreprocessLangRE) ||
-          [])[1] as SFCAsyncStyleCompileOptions['preprocessLang']
+          [])[1] as SFCAsyncStyleCompileOptions["preprocessLang"];
 
-        const result = isVueStyle
-          ? css
-          : await compileCss(
-              root,
-              id,
-              {
-                id: '',
-                source: css,
-                filename: id,
-                scoped: false,
-                modules: cssModuleRE.test(id),
-                preprocessLang,
-                preprocessOptions,
-                modulesOptions
-              },
-              true
-            )
+        const result = isVueStyle ? css : await compileCss(
+          root,
+          id,
+          {
+            id: "",
+            source: css,
+            filename: id,
+            scoped: false,
+            modules: cssModuleRE.test(id),
+            preprocessLang,
+            preprocessOptions,
+            modulesOptions,
+          },
+          true,
+        );
 
-        let modules: SFCStyleCompileResults['modules']
-        if (typeof result === 'string') {
-          css = result
+        let modules: SFCStyleCompileResults["modules"];
+        if (typeof result === "string") {
+          css = result;
         } else {
           if (result.errors.length) {
-            console.error(`[vite] error applying css transforms: `)
-            result.errors.forEach(console.error)
+            console.error(`[vite] error applying css transforms: `);
+            result.errors.forEach(console.error);
           }
-          css = result.code
-          modules = result.modules
+          css = result.code;
+          modules = result.modules;
         }
 
         // process url() - register referenced files as assets
         // and rewrite the url to the resolved public path
         if (urlRE.test(css)) {
-          const fileDir = path.dirname(id)
+          const fileDir = path.dirname(id);
           css = await rewriteCssUrls(css, async (rawUrl) => {
             const file = path.posix.isAbsolute(rawUrl)
               ? path.join(root, rawUrl)
-              : path.join(fileDir, rawUrl)
+              : path.join(fileDir, rawUrl);
             const { fileName, content, url } = await resolveAsset(
               file,
               root,
               publicBase,
               assetsDir,
-              inlineLimit
-            )
+              inlineLimit,
+            );
             if (fileName && content) {
-              assets.set(fileName, content)
+              assets.set(fileName, content);
             }
             debug(
               `url(${rawUrl}) -> ${
-                url.startsWith('data:') ? `base64 inlined` : `url(${url})`
-              }`
-            )
-            return url
-          })
+                url.startsWith("data:") ? `base64 inlined` : `url(${url})`
+              }`,
+            );
+            return url;
+          });
         }
 
-        styles.set(id, css)
+        styles.set(id, css);
         return {
           code: modules
             ? dataToEsm(modules, { namedExports: true })
             : (cssCodeSplit
-                ? // If code-splitting CSS, inject a fake marker to avoid the module
-                  // from being tree-shaken. This preserves the .css file as a
-                  // module in the chunk's metadata so that we can retrieve them in
-                  // renderChunk.
-                  `${cssInjectionMarker}()\n`
-                : ``) + `export default ${JSON.stringify(css)}`,
+              ? // If code-splitting CSS, inject a fake marker to avoid the module
+              // from being tree-shaken. This preserves the .css file as a
+              // module in the chunk's metadata so that we can retrieve them in
+              // renderChunk.
+                `${cssInjectionMarker}()\n`
+              : ``) + `export default ${JSON.stringify(css)}`,
           map: null,
           // #795 css always has side effect
-          moduleSideEffects: true
-        }
+          moduleSideEffects: true,
+        };
       }
     },
 
     async renderChunk(code, chunk) {
-      let chunkCSS = ''
+      let chunkCSS = "";
       for (const id in chunk.modules) {
         if (styles.has(id)) {
-          chunkCSS += styles.get(id)
+          chunkCSS += styles.get(id);
         }
       }
 
       if (cssCodeSplit) {
-        code = code.replace(cssInjectionRE, '')
+        code = code.replace(cssInjectionRE, "");
         // for each dynamic entry chunk, collect its css and inline it as JS
         // strings.
         if (chunk.isDynamicEntry && chunkCSS) {
-          chunkCSS = minifyCSS(chunkCSS)
+          chunkCSS = minifyCSS(chunkCSS);
           code =
             `let ${cssInjectionMarker} = document.createElement('style');` +
             `${cssInjectionMarker}.innerHTML = ${JSON.stringify(chunkCSS)};` +
             `document.head.appendChild(${cssInjectionMarker});` +
-            code
+            code;
         } else {
-          staticCss += chunkCSS
+          staticCss += chunkCSS;
         }
         return {
           code,
-          map: null
-        }
+          map: null,
+        };
       } else {
-        staticCss += chunkCSS
-        return null
+        staticCss += chunkCSS;
+        return null;
       }
     },
 
     async generateBundle(_options, bundle) {
       // minify css
       if (minify) {
-        staticCss = minifyCSS(staticCss)
+        staticCss = minifyCSS(staticCss);
       }
 
-      const cssFileName = `style.${hash_sum(staticCss)}.css`
+      // const cssFileName = `style.${hash_sum(staticCss)}.css`
 
-      bundle[cssFileName] = {
-        name: cssFileName,
-        isAsset: true,
-        type: 'asset',
-        fileName: cssFileName,
-        source: staticCss
-      }
+      // bundle[cssFileName] = {
+      //   name: cssFileName,
+      //   isAsset: true,
+      //   type: 'asset',
+      //   fileName: cssFileName,
+      //   source: staticCss
+      // }
 
-      registerAssets(assets, bundle)
-    }
-  }
-}
+      Object.keys(bundle).forEach((b) => {
+        if (!b.includes("runtime-dom.esm-bundler")) {
+          const t = b.split("/")[1].replace(".js", "");
+          if (t && !t.includes("runtime-dom.esm-bundler")) {
+            const t = b.split("/")[1].replace(".js", "");
+            const cssFileName = `${t}/${t}.css`;
+            bundle[cssFileName] = {
+              name: cssFileName,
+              isAsset: true,
+              type: "asset",
+              fileName: cssFileName,
+              source: staticCss,
+            };
+          }
+        }
+      });
 
-let CleanCSS: any
+      registerAssets(assets, bundle);
+    },
+  };
+};
+
+let CleanCSS: any;
 
 function minifyCSS(css: string) {
-  CleanCSS = CleanCSS || require('clean-css')
-  const res = new CleanCSS({ level: 2, rebase: false }).minify(css)
+  CleanCSS = CleanCSS || require("clean-css");
+  const res = new CleanCSS({ level: 2, rebase: false }).minify(css);
 
   if (res.errors && res.errors.length) {
-    console.error(chalk.red(`[vite] error when minifying css:`))
-    console.error(res.errors)
+    console.error(chalk.red(`[vite] error when minifying css:`));
+    console.error(res.errors);
   }
 
   if (res.warnings && res.warnings.length) {
-    console.error(chalk.yellow(`[vite] warnings when minifying css:`))
-    console.error(res.warnings)
+    console.error(chalk.yellow(`[vite] warnings when minifying css:`));
+    console.error(res.warnings);
   }
 
-  return res.styles
+  return res.styles;
 }
